b0d3fa50822cf48b3d3f4d58a494b3f9a1c6dbbf
vengatesan kandasamy <vengatesankandasamy@vengatesans-Mac-mini.local>
2023-11-26 21:08:12 +0530

    type

--- src/services/battery/battery.service.ts
+++ src/services/battery/battery.service.ts
@@ -4,8 +4,8 @@ import { BatteryType } from '../../model/battery/type/battery.type';
 export class BatteryService {
   constructor() {}
 
-  public static getBattery = async () => {
-    return new Promise<unknown>(async (resolve, reject) => {
+  public static getBattery = async (): Promise<BatteryType[]> => {
+    return new Promise<BatteryType[]>(async (resolve, reject) => {
       try {
         const results = await AppDataSource.getRepository(BatteryEntity).find();
         resolve(results);
@@ -14,8 +14,8 @@ export class BatteryService {
       }
     });
   };
-  public static postBattery = async (data: BatteryType) => {
-    return new Promise<unknown>(async (resolve, reject) => {
+  public static postBattery = async (data: BatteryType): Promise<BatteryType> => {
+    return new Promise(async (resolve, reject) => {
       try {
         const { dealerName, batteryType, batterySerialNumber, status, createdDateTime, updatedDateTime } = data;
         const postRepository = AppDataSource.getRepository(BatteryEntity);
